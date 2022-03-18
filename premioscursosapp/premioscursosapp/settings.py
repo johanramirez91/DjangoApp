e29abe6f0a82b2b4c0986591b7af57616fe53211
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-03-18 17:40:03 -0500

    [fix] key ignored

--- premioscursosapp/premioscursosapp/settings.py
+++ premioscursosapp/premioscursosapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from secrets import SECRETKEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5t+b8c#5s=rv9@8s*x^yo%ikb6kv%evz=w^^ufz&b4q4m81^6!'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
