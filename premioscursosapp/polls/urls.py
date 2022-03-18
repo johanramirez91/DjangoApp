92b6bfb68ade821ccdd0089277671da2ce4adffd
Johan Ramirez <johan.ramirez@sofka.com.co>
2022-03-18 16:58:20 -0500

    [Added] views por clases y funciones

--- premioscursosapp/polls/urls.py
+++ premioscursosapp/polls/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "premioscursos"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="question"),
-    path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="votos"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"), #function based view
     ]
